3e12ef37f42d2f5fa5b5bd623ff957b61e825ad2
ManuelQuijada <manuelquijada2000@gmail.com>
2025-11-06 21:13:31 +0100

    Estructura docket compose

---
+++ backend/flashfly/src/main/java/com/flashfly_backend/flashfly/FlashflyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flashfly_backend.flashfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashflyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashflyBackendApplication.class, args);
+	}
+
+}
