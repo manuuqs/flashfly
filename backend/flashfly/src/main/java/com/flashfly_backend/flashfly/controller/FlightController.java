6196014c145fc60e6ab109eda2ad35f637469a47
Manuel Quijada <manuel.quijada.contractor@bbva.com>
2025-11-08 20:10:32 +0100

    añadir servicio de ofertas por fecha y destino

--- backend/flashfly/src/main/java/com/flashfly_backend/flashfly/controller/FlightController.java
+++ backend/flashfly/src/main/java/com/flashfly_backend/flashfly/controller/FlightController.java
@@ -1,6 +1,8 @@
 package com.flashfly_backend.flashfly.controller;
 
+import com.amadeus.exceptions.ResponseException;
 import com.amadeus.resources.FlightDestination;
+import com.amadeus.resources.FlightOfferSearch;
 import com.flashfly_backend.flashfly.service.AmadeusService;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,5 +20,17 @@ public class FlightController {
     @GetMapping("/destinations")
     public FlightDestination[] getFlightDestinations(@RequestParam String origin, @RequestParam int maxPrice) throws Exception {
         return amadeusService.getFlightDestinations(origin, maxPrice);
+    }
+
+    @GetMapping("/offers")
+    public FlightOfferSearch[] getFlightOffers(
+            @RequestParam String origin,
+            @RequestParam String destination,
+            @RequestParam String departureDate,
+            @RequestParam String returnDate,
+            @RequestParam int adults,
+            @RequestParam int max
+    ) throws ResponseException {
+        return amadeusService.searchFlightOffers(origin, destination, departureDate, returnDate, adults, max);
     }
 }
--- backend/flashfly/src/main/java/com/flashfly_backend/flashfly/service/AmadeusService.java
+++ backend/flashfly/src/main/java/com/flashfly_backend/flashfly/service/AmadeusService.java
@@ -2,7 +2,9 @@ package com.flashfly_backend.flashfly.service;
 
 import com.amadeus.Amadeus;
 import com.amadeus.Params;
+import com.amadeus.exceptions.ResponseException;
 import com.amadeus.resources.FlightDestination;
+import com.amadeus.resources.FlightOfferSearch;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -15,16 +17,38 @@ public class AmadeusService {
             @Value("${amadeus.api.key}") String apiKey,
             @Value("${amadeus.api.secret}") String apiSecret
     ) {
+        System.out.println("API KEY: " + apiKey);
+        System.out.println("API SECRET: " + apiSecret);
         this.amadeus = Amadeus.builder(
                 apiKey, apiSecret)
                 .build();
     }
 
     public FlightDestination[] getFlightDestinations(String origin, int maxPrice) throws Exception {
-        return amadeus.shopping.flightDestinations.get(
-                Params.with("origin", origin)
-                        .and("maxPrice", maxPrice)
-        );
+        try {
+            return amadeus.shopping.flightDestinations.get(
+                Params.with("origin", origin).and("maxPrice", maxPrice)
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public FlightOfferSearch[] searchFlightOffers(String origin, String destination, String departureDate, String returnDate, int adults, int max) throws ResponseException, ResponseException {
+        try {
+            return amadeus.shopping.flightOffersSearch.get(
+                Params.with("originLocationCode", origin)
+                      .and("destinationLocationCode", destination)
+                      .and("departureDate", departureDate)
+                      .and("returnDate", returnDate)
+                      .and("adults", adults)
+                      .and("max", max)
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 
 
