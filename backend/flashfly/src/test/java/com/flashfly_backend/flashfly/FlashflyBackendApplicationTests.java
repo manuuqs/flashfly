4c89613c3120dfa982c630358c0454fec85140f1
ManuelQuijada <manuelquijada2000@gmail.com>
2025-11-16 19:28:01 +0100

    cambiar bbdd por mongo 2

--- backend/flashfly/src/test/java/com/flashfly_backend/flashfly/FlashflyBackendApplicationTests.java
+++ backend/flashfly/src/test/java/com/flashfly_backend/flashfly/FlashflyBackendApplicationTests.java
@@ -2,12 +2,17 @@ package com.flashfly_backend.flashfly;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-class FlashflyBackendApplicationTests {
+@TestPropertySource(properties = {
+        "spring.data.mongodb.uri=mongodb://localhost:27017/test",
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration"
+})
+public class FlashflyBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Test sin necesidad de MongoDB
+    }
 }
