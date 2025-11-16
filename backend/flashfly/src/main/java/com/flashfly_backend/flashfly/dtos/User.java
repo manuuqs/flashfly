9ffb8f3769dc0fb62a5922a0fa2bbbd222e87576
ManuelQuijada <manuelquijada2000@gmail.com>
2025-11-16 19:12:58 +0100

    cambiar bbdd por mongo

--- backend/flashfly/src/main/java/com/flashfly_backend/flashfly/dtos/User.java
+++ backend/flashfly/src/main/java/com/flashfly_backend/flashfly/dtos/User.java
@@ -1,28 +1,22 @@
 package com.flashfly_backend.flashfly.dtos;
 
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "users")
+@Document(collection = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
 
-    @Column(unique = true, nullable = false)
     private String email;
-
     private String name;
-
-    private String password; // puede ser null si el usuario se registra solo con Google
-
-    private String picture; // URL de perfil Google, opcional
-
+    private String password;
+    private String picture;
 }
